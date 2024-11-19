fae954823e9ce7970551c5dd7e0f04dbdfdc435a
ElaineNguyen1199 <elainenguyen@unomaha.edu>
2024-11-19 12:59:30 -0600

    television code has been moved to here from thonny

--- television.py
+++ television.py
@@ -1,3 +1,35 @@
-print("tv")
-print("tv")
-print("tv")
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self, status, muted, volume, channel):
+        pass
+
+    #         self.__status = False
+    #         self.__muted = False
+    #         self.__volume =
+    #         self.__channel =
+
+    def power(self):
+        pass
+
+    def mute(self):
+        pass
+
+    def channel_up(self):
+        pass
+
+    def channel_down(self):
+        pass
+
+    def volume_up(self):
+        pass
+
+    def volume_down(self):
+        pass
+
+    def __str__(self):
+        # Power = [status], Channel = [channel], Volume = [volume]
+        pass
