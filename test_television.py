d53ff93171f594d6791dc36e2e047335cfa9ca0e
ElaineNguyen1199 <elainenguyen@unomaha.edu>
2024-11-22 14:40:57 -0600

    finished all the parts for testing in unittest

--- test_television.py
+++ test_television.py
@@ -1,2 +1,81 @@
-print("Testing to see if it goes into the test branch in git hub")
-print("HEllo")
+import unittest
+from television import *
+
+class MyTestCase(unittest.TestCase):
+    def test_init(self):
+        tv = Television()
+        assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+
+    def test_power(self):
+        tv = Television()
+        tv.power()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+        tv.power()
+        tv = Television()
+        assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+
+    def test_mute(self):
+        tv = Television()
+        tv.power()
+        tv.volume_up()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 1"
+        tv.mute()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+        tv.mute()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 1"
+        tv.power()
+        tv.mute()
+        assert str(tv) == "Power = False, Channel = 0, Volume = 1"
+
+    def test_channel_up(self):
+        tv = Television()
+        tv.channel_up()
+        assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+        tv.power()
+        tv.channel_up()
+        assert str(tv) == "Power = True, Channel = 1, Volume = 0"
+        tv.channel_up()
+        tv.channel_up()
+        tv.channel_up()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+
+    def test_channel_down(self):
+        tv = Television()
+        tv.channel_down()
+        assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+        tv.power()
+        tv.channel_down()
+        assert str(tv) == "Power = True, Channel = 3, Volume = 0"
+        tv.channel_down()
+        tv.channel_down()
+        tv.channel_down()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+
+    def test_volume_up(self):
+        tv = Television()
+        tv.volume_up()
+        assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+        tv.power()
+        tv.volume_up()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 1"
+        tv.mute()
+        tv.volume_up()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 2"
+        tv.volume_up()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 2"
+
+    def test_volume_down(self):
+        tv = Television()
+        tv.volume_down()
+        assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+        tv.power()
+        tv.volume_up()
+        tv.volume_up()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 2"
+        tv.volume_down()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 1"
+        tv.mute()
+        tv.volume_down()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 0"
+        tv.volume_down()
+        assert str(tv) == "Power = True, Channel = 0, Volume = 0"
